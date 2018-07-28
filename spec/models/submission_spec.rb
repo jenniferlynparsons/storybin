4afb278a4687714844edf82a5706a39e39740291
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2018-07-28 13:46:50 -0400

    finish this round of template cleanup

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -2,18 +2,19 @@
 #
 # Table name: submissions
 #
-#  id           :bigint(8)        not null, primary key
-#  cover_letter :text
-#  active       :boolean
-#  simultaneous :boolean
-#  reprint      :boolean
-#  reprint_date :date
-#  status       :string
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#  user_id      :integer
-#  work_id      :integer
-#  issue_id     :integer
+#  id                  :bigint(8)        not null, primary key
+#  cover_letter        :text
+#  active              :boolean
+#  simultaneous        :boolean
+#  reprint             :boolean
+#  reprint_date        :date
+#  status              :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  user_id             :integer
+#  work_id             :integer
+#  issue_id            :integer
+#  reprint_publication :text
 #
 
 require 'rails_helper'
